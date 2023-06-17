062632368791df9bfdd723c9a2c7a24d17f308ea
Kavita Gupta <kavitagupta.gupta123@gmail.com>
2023-06-18 00:59:51 +0530

    wip

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
+
 
 // Replace <mongodb-link> with the MongoDB connection string provided by your hosting service
-const url = 'mongodb+srv://kavita:kavi89198919@kika8919.kzzynyd.mongodb.net/<banking_system>?retryWrites=true&w=majority';
-const dbName = 'banking_system'; // Replace with your preferred database name
+const url =
+  "mongodb+srv://kavita:kavi89198919@kika8919.kzzynyd.mongodb.net/banking_system?retryWrites=true&w=majority";
+const dbName = "banking_system"; // Replace with your preferred database name
+
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
 // Function to connect to the MongoDB database
 async function connectToDatabase() {
-  const client = new MongoClient(url, { useUnifiedTopology: true });
-
   try {
     await client.connect();
-    console.log('Connected to the database');
+    console.log("Connected to the database");
 
     const db = client.db(dbName);
     return db;
   } catch (error) {
-    console.error('Error connecting to the database:', error);
+    console.error("Error connecting to the database:", error);
     throw error;
   }
 }
@@ -23,13 +25,13 @@ async function connectToDatabase() {
 // Example function to insert a new customer into the database
 async function insertCustomer(customerData) {
   const db = await connectToDatabase();
-  const customersCollection = db.collection('customers');
+  const customersCollection = db.collection("customers");
 
   try {
     const result = await customersCollection.insertOne(customerData);
-    console.log('Customer inserted:', result.insertedId);
+    console.log("Customer inserted:", result.insertedId);
   } catch (error) {
-    console.error('Error inserting customer:', error);
+    console.error("Error inserting customer:", error);
     throw error;
   }
 }
@@ -37,22 +39,22 @@ async function insertCustomer(customerData) {
 // Example function to retrieve all customers from the database
 async function getCustomers() {
   const db = await connectToDatabase();
-  const customersCollection = db.collection('customers');
+  const customersCollection = db.collection("customers");
 
   try {
     const customers = await customersCollection.find().toArray();
-    console.log('All customers:', customers);
+    console.log("All customers:", customers);
   } catch (error) {
-    console.error('Error retrieving customers:', error);
+    console.error("Error retrieving customers:", error);
     throw error;
   }
 }
 
 // Example usage
 const newCustomer = {
-  name: 'John Doe',
-  email: 'johndoe@example.com',
-  balance: 1000
+  name: "John Doe",
+  email: "johndoe@example.com",
+  balance: 1000,
 };
 
 insertCustomer(newCustomer);
